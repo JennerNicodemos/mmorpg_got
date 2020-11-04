1b266a5a2e1b69ef4c2e4bad8948a037c1f21768
Jenner Nicodemos <jenner.nicodemos@gmail.com>
2020-11-04 15:20:20 -0300

    Configurada a conexão com o Banco de Dados MongoDB

---
+++ config/dbConnection.js
@@ -0,0 +1,21 @@
+// Importar o MongoDB
+let mongo = require('mongodb')
+
+let connMongoDB = function() {
+    console.log('Entrou na função de conexão')
+    let db = new mongo.Db (
+        'got',
+        new mongo.Server(
+            'localhost', //String contendp o endereço do servidor
+            27017, //porta de conexão
+            {}
+        ),
+        {}
+    )
+    return db
+}
+
+
+module.exports = function() {
+    return connMongoDB
+}
