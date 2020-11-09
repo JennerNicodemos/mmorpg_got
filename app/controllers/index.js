d4a1db5fd93be69671f1cbf5ce1a216d38245886
Jenner Nicodemos <jenner.nicodemos@gmail.com>
2020-11-09 12:54:36 -0300

    Criado o controller de autenticação

--- app/controllers/index.js
+++ app/controllers/index.js
@@ -1,3 +1,20 @@
 module.exports.index = function(application, req, res) {
-    res.render('index');
+    res.render('index', { validacao: {}, dadosForm: {} });
+}
+
+module.exports.autenticar = function(application, req, res) {
+    
+    let dadosForm = req.body
+
+    req.assert("usuario", "Usuário não pode ser vazio").notEmpty()
+    req.assert("senha", "Senha não pode ser vazia").notEmpty()
+
+    let erros = req.validationErrors()
+
+    if(erros) {
+        res.render("index", { validacao: erros, dadosForm: dadosForm })
+        return
+    }
+
+    res.send("Tudo certo para criar a sessão")
 }
