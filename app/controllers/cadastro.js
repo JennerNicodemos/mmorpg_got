a8729cf95ea8aee8a4e6d2fa06e92c474a2cb58c
Jenner Nicodemos <jenner.nicodemos@gmail.com>
2020-11-30 11:48:38 -0300

    Gerado pontos de habilidade, súditos, e modedas

--- app/controllers/cadastro.js
+++ app/controllers/cadastro.js
@@ -20,8 +20,11 @@ module.exports.cadastrar = function(application, req, res) {
 
     let connection = application.config.dbConnection
     let UsuariosDAO = new application.app.models.UsuariosDAO(connection)
+    let JogoDAO = new application.app.models.JogoDAO(connection)
     
     UsuariosDAO.inserirUsuario(dadosForm)
+    JogoDAO.gerarParametros(dadosForm.usuario)
+    // geração dos parametros
 
     res.send('Podemos cadastrar')
 }
